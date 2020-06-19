f4eeb16a62a1807aeb6ec55dc9f0a3fd3cb754fa
MithransRepos <mithran.itsme@gmail.com>
2020-06-19 19:44:52 +0530

    Initial commit

---
+++ BoilterPlateApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BoilterPlateApp
+//
+//  Created by MithranN on 19/06/20.
+//  Copyright © 2020 MithranN. All rights reserved.
+//
+
+import UIKit
+import MithranSwiftKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
